1c666216e123a90e7ba4929bd6694851c223e96c
= <desuko@gmail.com>
2016-08-18 01:56:21 +0200

    Events binding ready ?

--- recurtion.js
+++ recurtion.js
@@ -11,16 +11,26 @@ var Recrution = function( name, $dom ){
 }
 
 Recurtion.prototype.addRecurtion = function( recurtion ){
+	console.log('Adding a new recurtion : ' + recurtion.name);
 	recurtion.owner = this;
 	this.recurtion[recurtion.name] = recurtion;
 }
 
 Recurtion.prototype.addState = function( args ){
-	console.log('Going to state : ' + state);
+	console.log('Adding a new state : ' + args.name);
 	this.states[args.name] = args.setup;
 }
 
 Recurtion.prototype.gotoState = function( state ){
 	console.log('Going to state : ' + state);
 	this.states[state]();
+}
+
+Recurtion.prototype.bindEvents = function( bind ){
+	bind(this);
+}
+
+Recurtion.prototype.get = function( recurtion ){
+	console.log('Getting : ' + recurtion);
+	return this.recurtions[recurtion] || undefined;
 }
